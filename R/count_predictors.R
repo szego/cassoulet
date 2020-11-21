5315aa3b440ac98befaffc597d7feac4af627d3d
Antonio Vargas <15042864+szego@users.noreply.github.com>
2020-11-21 15:41:47 -0800

    Standardize count_predictors() argument names.

--- R/count_predictors.R
+++ R/count_predictors.R
@@ -3,22 +3,22 @@
 #' @description
 #' Returns the number of variables with the "predictor" role in a recipe or workflow.
 #'
-#' @param x A `recipe` object or a `workflow` object with an attached recipe.
+#' @param object A `recipe` object or a `workflow` object with an attached recipe.
 #'
 #' @return An integer.
 #' @export
-count_predictors <- function(x) {
-  UseMethod("count_predictors", x)
+count_predictors <- function(object) {
+  UseMethod("count_predictors", object)
 }
 
 #' @describeIn count_predictors
 #' @export
-count_predictors.recipe <- function(x) {
-  sum(x$var_info$role == "predictor")
+count_predictors.recipe <- function(object) {
+  sum(object$var_info$role == "predictor")
 }
 
 #' @describeIn count_predictors
 #' @export
-count_predictors.workflow <- function(x) {
-  sum(workflows::pull_workflow_preprocessor(x)$var_info$role == "predictor")
+count_predictors.workflow <- function(object) {
+  sum(workflows::pull_workflow_preprocessor(object)$var_info$role == "predictor")
 }
