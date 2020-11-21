8da886fbf4884b53bb674fa9f45b3d72ff7a3aff
Antonio Vargas <15042864+szego@users.noreply.github.com>
2020-11-20 22:50:31 -0800

    Revert "Remove unnecessary dot prefix from arguments of predict_resamples()."
    This reverts commit 11bcf124486cadda3bda58c9ae82ca35f01caed4.

--- R/predict_resamples.R
+++ R/predict_resamples.R
@@ -4,27 +4,26 @@
 #' Fits a list of `workflow` objects to resampled data. Its output can be used to train
 #' higher models in a stack.
 #'
-#' @param workflows A named list of `workflow` objects.
+#' @param .workflows A named list of `workflow` objects.
 #' @param resamples A resample `rset` created from an `rsample` function such as `rsample::vfold_cv()`.
 #'
 #' @return A `data.frame` with one column for each fit's predictions.
 #' @export
-predict_resamples <- function(workflows, resamples) {
-  if(is.null(names(workflows)))
-    names(workflows) <- 1:length(workflows)
+predict_resamples <- function(.workflows, resamples) {
+  if(is.null(names(.workflows)))
+    names(.workflows) <- 1:length(.workflows)
 
   purrr::map_dfc(
-    1:length(workflows),
-    function(i)
-      workflows[[i]] %>%
+    1:length(.workflows),
+    ~ .workflows[[.x]] %>%
       tune::fit_resamples(resamples, control = tune::control_resamples(save_pred = TRUE)) %>%
       tune::collect_predictions() %>%
       dplyr::select(dplyr::starts_with(".pred")[1], ncol(.)) %>%
       stats::setNames(c(
-        names(workflows)[i],
-        paste0(names(.)[ncol(.)], "_", i)
+        names(.workflows)[.x],
+        paste0(names(.)[ncol(.)], "_", .x)
       ))
   ) %>%
-    dplyr::select(ncol(.), dplyr::all_of(names(workflows))) %>%
-    stats::setNames(c(sub("_[0-9]+$", "", names(.)[1]), names(workflows)))
+    dplyr::select(ncol(.), dplyr::all_of(names(.workflows))) %>%
+    stats::setNames(c(sub("_[0-9]+$", "", names(.)[1]), names(.workflows)))
 }
