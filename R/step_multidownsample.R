84c4fa02a8f4e80f4c07e4d3adb07472a0337c49
Antonio Vargas <15042864+szego@users.noreply.github.com>
2020-11-20 22:06:19 -0800

    Remove unnecessary dot prefix in arguments of step_multidownsample().

--- R/step_multidownsample.R
+++ R/step_multidownsample.R
@@ -4,7 +4,7 @@
 #' `step_multidownsample()` replicates a given recipe and adds a
 #' `themis::step_downsample()` step to each replicate.
 #'
-#' @param .recipe A `recipe` object.
+#' @param recipe A `recipe` object.
 #' @param ... One or more selector functions to choose which variable is used
 #' to sample the data. See `recipes::selections()` for more details. The
 #' selection should result in single factor variable.
@@ -14,7 +14,7 @@
 #' @return A list of replicates of the provided recipe, each with a new
 #' `themis::step_downsample()` step added to the sequence of existing steps (if any).
 #' @export
-step_multidownsample <- function(.recipe, ..., .replicates) {
+step_multidownsample <- function(recipe, ..., .replicates) {
   x <- rlang::enquos(...)
-  purrr::map(1:.replicates, ~ themis::step_downsample(.recipe, !!!x))
+  purrr::map(1:.replicates, ~ themis::step_downsample(recipe, !!!x))
 }
