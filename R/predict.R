10b6c1037b38c97fb7290b2804fda2c59e5586ea
Antonio Vargas <15042864+szego@users.noreply.github.com>
2020-11-20 22:27:42 -0800

    Rename arguments of predict.wflist().

--- R/predict.R
+++ R/predict.R
@@ -7,30 +7,30 @@ stats::predict
 #' @description
 #' Makes predictions from multiple `workflow` objects.
 #'
-#' @param .fits A named list of fitted `workflow` objects created by `wf_list()`.
+#' @param workflows A named list of fitted `workflow` objects created by `wf_list()`.
 #' @param new_data A `data.frame` containing data with which to predict.
 #' @param mode Either "regression" or "classification".
 #'
 #' @return A `data.frame` with one column for each fit's predictions.
 #' @export
-predict.wflist <- function(.fits, new_data, mode) {
+predict.wflist <- function(workflows, new_data, mode) {
   if(mode == "regression") {
     type <- "numeric"
   } else if(mode == "classification") {
     type <- "prob"
   } else {
-    stop("Argument 'mode' must be one of the following: regression, classification")
     stop("`mode` must be one of the following: regression, classification")
   }
 
-  if(is.null(names(.fits)))
-    names(.fits) <- 1:length(.fits)
+  if(is.null(names(workflows)))
+    names(workflows) <- 1:length(workflows)
 
   purrr::map_dfc(
-    1:length(.fits),
-    ~ .fits[[.x]] %>%
+    1:length(workflows),
+    function(i)
+      workflows[[i]] %>%
       stats::predict(new_data, type = type) %>%
       dplyr::select(dplyr::starts_with(".pred")[1]) %>%
-      stats::setNames(names(.fits)[.x])
+      stats::setNames(names(workflows)[i])
   )
 }
