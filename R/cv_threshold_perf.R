08a88a2fe53a39a6d76b1898b444595fa07f67ea
Antonio Vargas <15042864+szego@users.noreply.github.com>
2020-11-24 21:14:24 -0800

    Document cv_threshold_perf().

--- R/cv_threshold_perf.R
+++ R/cv_threshold_perf.R
@@ -1,3 +1,28 @@
+#' Title Generate cross-validated performance metrics across probability thresholds
+#'
+#' @description
+#' This function calculates out-of-sample performance characteristics for class
+#' probability thresholds for each provided split.
+#'
+#' @details
+#' For each training-testing pair, this function fits a model on the training
+#' set and generates class probability predictions for the testing set.
+#' `probably::threshold_perf()` is called on those out-of-sample predictions.
+#'
+#' See `?probably::threshold_perf` for details.
+#'
+#' @param object A `workflow` object.
+#' @param resamples A resample `rset` created from an `rsample` function such
+#' as `rsample::vfold_cv()`.
+#' @param outcome A character string containing the column identifier for the
+#' outcome variable.
+#' @param thresholds A numeric vector of values for the probability threshold.
+#' If unspecified, a series of values between 0.5 and 1.0 are used. Note: if
+#' this argument is used, it must be named.
+#' @param ... Additional arguments passed on to `probably::threshold_perf()`.
+#'
+#' @return
+#' @export
 cv_threshold_perf <- function(object, resamples, outcome, thresholds = NULL, ...) {
   if(!("workflow" %in% class(object)))
     stop("`object` must be a workflow.")
