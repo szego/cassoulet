0ea480ebb02ac61c0e63ce8a818c48696494cb30
Antonio Vargas <15042864+szego@users.noreply.github.com>
2020-11-24 21:36:38 -0800

    Document collect_metrics().

--- R/collect_metrics.R
+++ R/collect_metrics.R
@@ -2,6 +2,21 @@
 #' @export
 tune::collect_metrics
 
+#' Summarize threshold metrics across folds
+#'
+#' @description
+#' This function calculates means and standard deviations for each
+#' metric at each threshold value across all folds.
+#'
+#' See `?cv_threshold_perf` and `probably::threshold_perf` for details.
+#'
+#' Can be piped into `autoplot()`.
+#'
+#' @param x The data.frame produced by `cv_threshold_perf()`.
+#' @param ... Not currently used.
+#'
+#' @return A `data.frame` with class `cv_threshold_results`.
+#' @export
 collect_metrics.cv_thresholds <- function(x, ...) {
   out <-
     x %>%
